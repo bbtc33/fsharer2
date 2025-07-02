469fbb5c6ec97f798d4bbd618ca7563338a78e52
bbtc33 <maxyin3872@gmail.com>
2025-07-02 11:55:35 -0400

    Add modified files

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,14 @@
-from flask import Flask, flash, request, redirect, render_template, send_from_directory, url_for, jsonify
+from flask import Flask, flash, request, redirect, render_template, send_file, url_for, jsonify
 from flask_pymongo import PyMongo
-from gridfs import GridFS
+from gridfs import GridFS, GridFSBucket
 from werkzeug.utils import secure_filename
-import os, random
+import os, random, io, qrcode, base64
 import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 from bson import ObjectId
 from io import BytesIO
+from pymongo.errors import DuplicateKeyError
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
@@ -36,6 +38,34 @@ def make_link(base_url, filename):
     link = delete_right + "/uploads/" + filename
     return link
 
+def generate_qrcode(link):
+    qr = qrcode.QRCode(
+        box_size=10,
+        border=4
+    )
+    qr.add_data(link)
+    qr.make(fit=True)
+    img = qr.make_image(fill="black", back_color=(231,220,188))
+
+    img_io = io.BytesIO()
+    img.save(img_io, format="PNG")
+    img_io.seek(0)
+    qr_base64 = base64.b64encode(img_io.getvalue()).decode("utf-8")
+
+    return qr_base64
+
+def purge_files():
+    fs = GridFSBucket(mongo.db)
+    cutoff = datetime.utcnow() - timedelta(hours=12)
+    to_delete = mongo.db["fs.files"].find({"time":{"$lt": cutoff}})
+
+    for file in to_delete:
+        fs.delete(file["_id"])
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(purge_files, "interval", minutes=5)
+scheduler.start()
+
 @app.route("/")
 def home_page():
     return render_template('index.html')
@@ -44,31 +74,31 @@ def home_page():
 def submit_file():
     if request.method == 'POST':
         if "file" not in request.files:
-            flash("No file provided")
+            #flash("No file provided") #Needs fix
             return redirect(request.url)
         file = request.files['file']
         if file.filename == '':
-            flash("No file selected")
+            #flash("No file selected") #Needs fix
             return redirect(request.url)
 
         filename_array = file.filename.rsplit('.', 1)
         if len(filename_array) > 1:
             extension = filename_array[1].lower() #get extension
         else:
-            extension = ".txt" #generic extension
+            extension = "txt" #generic extension
         if app.config['BANNED_EXTENSIONS'] in extension:
-            extension = ".txt" #replace XSS vuln with generic extension
+            extension = "txt" #replace XSS vuln with generic extension
 
         attempts = 0
 
         #attempt to give the file a random name. Keep retrying if duplicate key, with a limit
         while attempts < 10:
             rand = get_random_string()
-            filename = secure_filename(rand + extension)
+            filename = secure_filename(rand + "." + extension)
 
             #Use mongodb instead
             try:
-                file_id = mongofs.put(file, filename=filename, _id=rand time=datetime.utcnow())
+                file_id = mongofs.put(file, filename=filename, _id=rand, time=datetime.utcnow())
                 attempts = 10
             except DuplicateKeyError:
                 attempts += 1
@@ -78,19 +108,21 @@ def submit_file():
 
         link = make_link(request.base_url, filename)
 
-    return render_template('success.html', link=link)
+    return render_template('success.html', link=link, qrcode=generate_qrcode(link))
 
 @app.route("/text", methods=['POST'])
 def submit_text():
     if request.method == 'POST':
         if "text" not in request.form:
-            flash("No text provided")
+            #flash("No text provided")
             return redirect(request.url)
         text = request.form['text']
 
         if not text:
-            flash("No text provided")
+            #flash("No text provided")
             return redirect(request.url)
+
+        file = text.encode("utf-8")
 
         #try to get random name and save to mongodb
         attempts = 0
@@ -109,14 +141,14 @@ def submit_text():
 
         link = make_link(request.base_url, filename)
 
-    return render_template('success.html', link=link)
+    return render_template('success.html', link=link, qrcode=generate_qrcode(link))
 
 @app.route("/uploads/<filename>")
 def uploaded_file(filename):
     if "." + app.config['BANNED_EXTENSIONS'] in filename:
         return "<h1>403 Forbidden</h1>", 403
-    file = fs.get(filename.rsplit(".", 1)[0])
-    return send_file(BytesIO(file.read(), attachment_filename=file.filename, as_attachment=True))
+    file = mongofs.get(filename.rsplit(".", 1)[0])
+    return send_file(BytesIO(file.read()), download_name=file.filename, as_attachment=False)
     #return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
